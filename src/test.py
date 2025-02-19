42b940b2fbfcb2524483dce9ae584879bd515788
Javi <javier.gijon02@gmail.com>
2025-02-19 11:32:51 +0100

    Se modifican imports para ser coherentes

--- src/test.py
+++ src/test.py
@@ -1,9 +1,10 @@
-import threading
+
 import sys
-audio_path = sys.argv[1]
-output_folder = "../audiochunks_volume"
 from utils.split_audio import split_audio
 from utils.transcribe_audio import transcribe_audio
+
+audio_path = sys.argv[1]
+output_folder = "../audiochunks_volume"
 
 split_audio(audio_path , output_folder)
 transcribe_audio(output_folder)
