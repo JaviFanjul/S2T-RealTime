c3afa8e867b9206552966ae8d503d87d502b8db9
Javi <javier.gijon02@gmail.com>
2025-02-19 11:56:40 +0100

    Se incluye longitud de chunk en config

--- src/test.py
+++ src/test.py
@@ -1,6 +1,6 @@
 from utils.split_audio import split_audio
 from utils.transcribe_audio import transcribe_audio
-from utils.config import audiopath, chunkpath
+from utils.config import audiopath , chunkpath , chunk_length_ms
 
-split_audio(audiopath , chunkpath)
+split_audio(audiopath , chunkpath, chunk_length_ms)
 transcribe_audio(chunkpath)
